be8c4511369e9d07371fbb1abd6eff01d253ef88
Ezio121 <65387250+Ezio121@users.noreply.github.com>
2023-12-20 22:26:21 +0530

    Update server.lua
    Added Autosave functionality 
    Check Readme

--- server.lua
+++ server.lua
@@ -1,5 +1,5 @@
-local savedVehicles = {}
-
+local currentVehicles = {}
+local isAutoSaveEnabled = true
 function GetPlayerIdentifierByLicense(playerId)
     local identifiers = GetPlayerIdentifiers(playerId)
     for _, identifier in pairs(identifiers) do
@@ -27,6 +27,7 @@ end)
 -- end)
 
 -- Event to save the vehicle data to the database
+
 RegisterServerEvent("saveVehicleToDatabase")
 AddEventHandler("saveVehicleToDatabase", function(vehicleData)
     local playerIdentifier = GetPlayerIdentifierByLicense(source)
@@ -41,6 +42,56 @@ AddEventHandler("saveVehicleToDatabase", function(vehicleData)
         })
     end
 end)
+
+-- Event to update the auto save status
+RegisterServerEvent("updateAutoSaveStatus")
+AddEventHandler("updateAutoSaveStatus", function(newStatus)
+    isAutoSaveEnabled = newStatus
+    print("Auto vehicle saves status updated:", isAutoSaveEnabled)
+end)
+
+
+
+-- Event to update the auto save status
+RegisterServerEvent("updateAutoSaveStatus")
+AddEventHandler("updateAutoSaveStatus", function(newStatus)
+    isAutoSaveEnabled = newStatus
+    print("Auto vehicle saves status updated:", isAutoSaveEnabled)
+end)
+
+-- Event to handle the current vehicle data from the client
+RegisterServerEvent("CurrentVehicle")
+AddEventHandler("CurrentVehicle", function(vehicleData)
+    local player = source
+
+    -- Store the current vehicle data in the table using the player's server ID as the key
+    currentVehicles[player] = vehicleData
+end)
+
+-- Event to save the vehicle data to the database on player disconnect
+AddEventHandler("playerDropped", function(reason)
+    local playerServerId = source
+    print(playerServerId)
+    if currentVehicles[playerServerId] and isAutoSaveEnabled then
+        local vehicleData = currentVehicles[playerServerId]
+        local playerIdentifier = GetPlayerIdentifierByLicense(playerServerId)
+        print(playerIdentifier)
+        if playerIdentifier then
+            MySQL.Async.execute([[
+                REPLACE INTO saved_vehicles (player_id, vehicle_data)
+                VALUES (@identifier, @vehicleData)
+            ]], {
+                ["@identifier"] = playerIdentifier,
+                ["@vehicleData"] = json.encode(vehicleData)
+            })
+            print("Save Success")
+        end
+        currentVehicles[playerServerId] = nil  -- Clear the stored data after using it
+    else
+        print("AutoSave Disabled")
+    end
+end)
+
 
 RegisterServerEvent("getPlayerLicense")
 AddEventHandler("getPlayerLicense", function(playerServerId)
@@ -64,8 +115,6 @@ AddEventHandler("requestSavedVehicle", function()
     local player = source  
     local playerIdentifier = GetPlayerIdentifierByLicense(player)
     
-    print("Player Identifier:", playerIdentifier)
-
     MySQL.Async.fetchScalar([[
         SELECT vehicle_data FROM saved_vehicles WHERE player_id = @identifier
     ]], {
@@ -94,6 +143,5 @@ AddEventHandler("deleteVehicleFromDatabase", function(playerServerId)
         })
     end
 end)
-
 
 
