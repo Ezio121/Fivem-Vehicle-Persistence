69a36e9064f04d9f129f42f7f970a0274a8332c3
Ezio121 <65387250+Ezio121@users.noreply.github.com>
2023-12-20 22:25:45 +0530

    Update client.lua
    Added AutoSave Functionality 
    Check Readme

--- client.lua
+++ client.lua
@@ -6,6 +6,8 @@ Config = {
 
 local hasVehicleSaved = false
 local playerLicense = nil
+local isAutoSaveEnabled = true  -- Variable to track whether auto save is enabled
+local isInVehicle = false
 
 -- Function to save the vehicle
 function SaveVehicle()
@@ -17,15 +19,33 @@ function SaveVehicle()
         local health = GetEntityHealth(vehicle)
         local fuel = DecorGetFloat(vehicle, "fFuel")
         local model = GetEntityModel(vehicle)
-        local vehicleData = { x = pos.x, y = pos.y, z = pos.z, heading = heading, health = health, fuel = fuel , model = model }
+        local color1,color2 = GetVehicleColours(vehicle) 
+        local vehicleData = { x = pos.x, y = pos.y, z = pos.z, heading = heading, health = health, fuel = fuel , model = model, color1 = color1, color2 = color2 }
 
         TriggerServerEvent("saveVehicleToDatabase", vehicleData)
-        hasVehicleSaved = true
         notify("Vehicle saved!")
     else
         notify("You are not in a vehicle.")
     end
 end
+
+function updateServer()
+    local vehicle = GetVehiclePedIsIn(GetPlayerPed(-1), false)
+
+    if DoesEntityExist(vehicle) then
+        local pos = GetEntityCoords(vehicle)
+        local heading = GetEntityHeading(vehicle)
+        local health = GetEntityHealth(vehicle)
+        local fuel = DecorGetFloat(vehicle, "fFuel")
+        local model = GetEntityModel(vehicle)
+        local color1,color2 = GetVehicleColours(vehicle) 
+        local vehicleData = { x = pos.x, y = pos.y, z = pos.z, heading = heading, health = health, fuel = fuel , model = model, color1 = color1, color2 = color2 }
+        return vehicleData
+    else
+        return nil
+    end    
+end
+
 
 -- Function to respawn the player in the saved vehicle
 function RespawnPlayerInVehicle(vehicleData)
@@ -38,6 +58,7 @@ function RespawnPlayerInVehicle(vehicleData)
         end
 
         local vehicle = CreateVehicle(vehicleHash, vehicleData.x, vehicleData.y, vehicleData.z, vehicleData.heading, true, false)
+        SetVehicleColours(vehicle, vehicleData.color1, vehicleData.color2)
         TaskWarpPedIntoVehicle(PlayerPedId(), vehicle, -1)
 
         SetEntityHealth(vehicle, vehicleData.health)
@@ -53,20 +74,42 @@ function notify(msg)
     SetNotificationTextEntry("STRING")
     AddTextComponentString(msg)
     local notification = DrawNotification(false, false)
-    
+
     Citizen.Wait(2000)
     RemoveNotification(notification)
-    
 end
 
 -- Function to get the player's license and trigger the server event
 function GetPlayerLicense()
-    local serverId = GetPlayerServerId(PlayerId())
-    
-    if serverId then
-        TriggerServerEvent("getPlayerLicense", serverId)
+    local player = PlayerId()
+    local playerServerId = GetPlayerServerId(player)
+
+    TriggerServerEvent("getPlayerLicense", playerServerId)
+end
+
+-- Function to check if the player is in a vehicle
+function CheckInVehicle()
+    local playerPed = GetPlayerPed(-1)
+    local inVehicle = IsPedInAnyVehicle(playerPed, false)
+    if inVehicle then
+        if not isInVehicle then
+            isInVehicle = true
+            -- Player has entered a vehicle, trigger your custom event here
+            local vehicleData = updateServer()
+            TriggerServerEvent("CurrentVehicle", vehicleData)
+        end
+    else
+        isInVehicle = false
     end
 end
+
+-- Main loop to check for being in a vehicle
+Citizen.CreateThread(function()
+    while isAutoSaveEnabled do
+        Citizen.Wait(500)  -- Adjust the interval as needed
+        CheckInVehicle()
+    end
+end)
 
 -- Event to trigger saving the vehicle when the configured key is pressed
 RegisterCommand("savevehicle", function()
@@ -77,11 +120,19 @@ RegisterCommand("savevehicle", function()
     end
 end, false)
 
+-- Event to toggle auto save when the configured key is pressed
+RegisterCommand("toggleautosave", function()
+    isAutoSaveEnabled = not isAutoSaveEnabled
+    local status = isAutoSaveEnabled and "enabled" or "disabled"
+    notify("Auto vehicle saves are now " .. status .. ".")
+    TriggerServerEvent("updateAutoSaveStatus", isAutoSaveEnabled)
+end, false)
+
 -- Event to trigger deleting the vehicle entry from the database when the configured key is pressed
 
 RegisterCommand("deletevehicle", function()
-        TriggerServerEvent("deleteVehicleFromDatabase", GetPlayerServerId(PlayerId()))
-        notify("Vehicle entry deleted from the database.")
+    TriggerServerEvent("deleteVehicleFromDatabase", GetPlayerServerId(PlayerId()))
+    notify("Vehicle entry deleted from the database.")
 end, false)
 
 
@@ -89,36 +140,33 @@ local isDeleteKeyPressed = false
 
 -- Function to handle the key press
 Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
+while true do
+    Citizen.Wait(0)
 
-        if IsControlJustReleased(0, Config.DeleteVehicleKey) then
-            isDeleteKeyPressed = true
-        end
+    if IsControlJustReleased(0, Config.DeleteVehicleKey) then
+        isDeleteKeyPressed = true
     end
+end
 end)
 
 -- Event to check for the key press and trigger the server event
 Citizen.CreateThread(function()
-    while true do
-        Citizen.Wait(0)
+while true do
+    Citizen.Wait(0)
 
-        if isDeleteKeyPressed then
-            isDeleteKeyPressed = false  -- Reset the flag
+    if isDeleteKeyPressed then
+        isDeleteKeyPressed = false  -- Reset the flag
 
-            -- Trigger the server event to delete the vehicle entry
-            TriggerServerEvent("deleteVehicleFromDatabase", GetPlayerServerId(PlayerId()))
+        -- Trigger the server event to delete the vehicle entry
+        TriggerServerEvent("deleteVehicleFromDatabase", GetPlayerServerId(PlayerId()))
 
-            -- Display a notification
-            notify("Vehicle entry deleted from the database.")
-        end
+        -- Display a notification
+        notify("Vehicle entry deleted from the database.")
     end
+end
 end)
 
-
-
-
--- Event to request the saved vehicle data when player joins
+-- Event to request the saved vehicle data when the player joins
 AddEventHandler("playerSpawned", function()
     GetPlayerLicense()  -- Trigger the request for player license
 end)
@@ -139,3 +187,7 @@ RegisterNetEvent("respawnPlayerInVehicle")
 AddEventHandler("respawnPlayerInVehicle", function(vehicleData)
     RespawnPlayerInVehicle(vehicleData)
 end)
+
+
+
+
